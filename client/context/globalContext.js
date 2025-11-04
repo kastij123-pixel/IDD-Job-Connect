32a5284e2bb5e29b668723843fd8a2b5aaf9dc00
kastij123-pixel <kastij123@gmail.com>
2025-11-04 01:14:18 +0100

    Update globalContext.js

--- client/context/globalContext.js
+++ client/context/globalContext.js
@@ -10,7 +10,7 @@ import { useRouter } from "next/navigation";
 
 const GlobalContext = createContext();
 
-axios.defaults.baseURL = "http://localhost:8000";
+axios.defaults.baseURL = "/";
 axios.defaults.withCredentials = true;
 
 export const GlobalContextProvider = ({ children }) => {
